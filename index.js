a94c137d3083845bd0a86afe5b12f527264e1ab1
Christian Cueni <christian.cueni@gmail.com>
2015-11-17 20:56:47 +0100

    try reactive!

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var cheerio = require("cheerio");
 var request = require("request");
 var Promise = require("promise");
+var Rx = require("rx");
+var EventEmitter = require('events').EventEmitter;
 
 var site = 'http://www.gi.alaska.edu';
 var mainUrl = 'AuroraForecast';
@@ -24,29 +26,34 @@ var promiseRequest = function(url) {
     });
 };
 
-var getEuropeLink = function(data) {
+var getEuropeLink = function(data, emitter) {
     var link = null;
     var $ = cheerio.load(data);
+
     $('.map li a').each(function(i, element) {
         if ('href' in element.attribs && europeLinkRegex.test(element.attribs.href)) {
             link = site + element.attribs.href;
         }
     });
-    return link;
+
+    emitter.emit('pageLink', link);
 };
 
 var getForecast = function(data) {
-    var $ = cheerio.load(data);
-    var levels = $('.levels span');
+    var $ = cheerio.load(data),
+        levels = $('.levels span'),
+        title = $('.title h1.title');
+
     if (levels.length > 0) {
         var className = levels[0].attribs.class;
         var match = levelRegex.exec(className);
         if (match) {
-            return match[1];
+            return {title: title[0].children[0].data, forecast: match[1]};
         }
     }
     return null;
 };
+
 
 var getNextDay = function(data) {
     var $, link = null;
@@ -61,80 +68,74 @@ var getNextDay = function(data) {
     return link;
 };
 
-var getForecastPage = function(link, promise) {
 
-    var success, error;
-    var forecasts = [];
-    if (!promise) {
-        promise = new Promise(function(resolve, reject) {
-               success = resolve;
-                error = reject;
-        });
-    }
-
-    function pageFetcher(pageLink) {
-        promiseRequest(pageLink).then(function (data) {
-            var forecast = {};
-            forecast.value = getForecast(data);
-            forecast.url = pageLink;
-            forecast.next = getNextDay(data);
-            forecasts.push(forecast);
-            if (!forecast.next || forecasts.length === maxDays) {
-                success(forecasts);
-            } else {
-                pageFetcher(forecast.next);
-            }
-        }, function (err) {
-            error(err);
-        });
-    }
-
-    pageFetcher(link);
-
-    //var success, error;
-    //
-    //if (!promise) {
-    //    promise = new Promise(function(resolve, reject) {
-    //       success = resolve;
-    //        error = reject;
-    //    });
-    //}
-    //
-    //if (!forecasts) {
-    //    forecasts = [];
-    //}
-    //
-    //console.log(success);
-    //
-    //promiseRequest(link).then(function (data) {
-    //    var forecast = {};
-    //    forecast.value = getForecast(data);
-    //    forecast.url = link;
-    //    forecast.next = getNextDay(data);
-    //    forecasts.push(forecast);
-    //    if (!forecast.next || forecasts.length === maxDays) {
-    //        success(forecasts);
-    //    } else {
-    //        getForecastPage(forecast.next, promise, forecasts);
-    //    }
-    //}, function (err) {
-    //    error(err);
-    //});
-    //
-    return promise;
+var tweet = function(messsage) {
+    console.log(messsage);
 };
+
 
 var indexUrl = site + "/" + mainUrl;
 
 request(indexUrl, function(error, response, html) {
 
     if(!error){
-        var euLink = getEuropeLink(html);
-        getForecastPage(euLink).then(function(pageData) {
-            console.log(pageData);
-        }, function(err) {
-            console.log(err);
+        //var euLink = getEuropeLink(html);
+        var eventEmitter = new EventEmitter();
+
+        var source = Rx.Observable.fromEvent(
+            eventEmitter,
+            'pageLink',
+            function (link) { return link;});
+
+        //var pageLinkStream = Rx.Observable.fromPromise(getEuropeLink(html));
+
+        var pageContentStream = source
+            .flatMap(function(link) {
+                return Rx.Observable.fromPromise(promiseRequest(link));
+            });
+
+        var foreCastStream = pageContentStream
+            .map(function(content) {
+                return getForecast(content);
+            });
+
+        var forCastSum3Days = foreCastStream
+            .bufferWithCount(3,1);
+
+        var forCastSum2Days = foreCastStream
+            .bufferWithCount(2,1);
+
+        var pageNextLinkStream = pageContentStream
+            .map(function(content) {
+                var link = getNextDay(content);
+                eventEmitter.emit('pageLink', link);
+                return link;
+            });
+
+        pageNextLinkStream.subscribe(function(link) {
+            console.log(link);
         });
+
+        forCastSum3Days.subscribe(function(forecasts) {
+
+            var threeDaySum = parseInt(forecasts[0].forecast) + parseInt(forecasts[1].forecast)
+                            + parseInt(forecasts[2].forecast);
+
+            console.log("sliding window 3 days: " + threeDaySum);
+            if (threeDaySum > 12) {
+                tweet("Three day sum is " + threeDaySum + " on " + forecasts[0].title);
+            }
+        });
+
+        forCastSum2Days.subscribe(function(forecasts) {
+            var twoDaySum = parseInt(forecasts[0].forecast) + parseInt(forecasts[1].forecast);
+            console.log("sliding window 2 days: " + twoDaySum);
+            if (twoDaySum > 8) {
+                tweet("Two day sum is " + twoDaySum + " on " + forecasts[0].title);
+            }
+        }, 0);
+
+        getEuropeLink(html, eventEmitter);
 
     }
 });
